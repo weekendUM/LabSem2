e660b4d9c13e1d82145c9ad08aab91e75c7ec641
Schronk <damian.sirbu01@e-uvt>
2021-05-18 16:54:00 +0300

    pb1 templates

--- Lab/main.cpp
+++ Lab/main.cpp
@@ -1,121 +1,58 @@
 #include <iostream>
-#include <cstring>
-#include <string>
-#include <vector>
+#include <algorithm>
 
-class promotie
+class rational
 {
-	std::string nume;
-	int durata;
-	int zi, luna, an;
-	int suma;
+	int numarator;
+	int numitor;
 public:
-	promotie(std::string nume, const int& durata = 1,
-		const int& zi = 1, const int& luna = 1, const int& an = 2000,
-		const int& suma = 1000);
-	friend std::ostream& operator<<(std::ostream& out, const promotie& obj);
-	promotie operator++(int);
-
-	inline const int get_suma() const { return this->suma; }
-
-	friend void change_date(promotie& obj,
-		int zi, int luna, int an);
-
-	~promotie();
+	rational(const int& numarator, const int& numitor)
+	{
+		if (numitor == 0)
+		{
+			throw "Numitorul nu poate fi 0";
+		}
+		this->numarator = numarator;
+		this->numitor = numitor;
+	}
+	friend bool operator<(const rational& self, const rational& other);
+	friend bool operator==(const rational& self, const rational& other);
+	friend bool operator>(const rational& self, const rational& other);
+	friend std::ostream& operator<<(std::ostream& out, const rational& obj);
 };
 
-promotie::promotie(std::string nume, const int& durata, const int& zi, const int& luna, const int& an, const int& suma)
+template<class T>
+T maxim(const T& a, const T& b, const T& c)
 {
-	this->nume = nume;
-	this->durata = durata;
-	this->zi = zi;
-	this->luna = luna;
-	this->an = an;
-	this->suma = suma;
-}
-
-promotie promotie::operator++(int)
-{
-	this->durata++;
-	return *this;
-}
-
-promotie::~promotie()
-{
-	
-}
-
-void read(const int& n, promotie** objs)
-{
-	std::string nume;
-	int durata;
-	int zi, luna, an;
-	int suma;
-	for (int i = 0; i < n; i++)
-	{
-		std::cin.ignore();
-		std::getline(std::cin, nume);
-		std::cin >> durata;
-		std::cin >> zi >> luna >> an;
-		std::cin >> suma;
-		objs[i] = new promotie(nume, durata, zi, luna, an, suma);
-	}
-}
-
-void print_mins(int n, promotie** objs)
-{
-	int min = 1000000; //ma indoiesc ca suma poate fi negativa
-	std::vector<promotie> res;
-	int k = 0;
-	for (int i = 0; i < n; i++)
-	{
-		if (objs[i]->get_suma() < min)
-		{
-			min = objs[i]->get_suma();
-			res.clear();
-			res.push_back(*objs[i]);
-		}
-		else if (objs[i]->get_suma() == min)
-		{
-			res.push_back(*objs[i]);
-		}
-	}
-	for (auto el : res)
-	{
-		std::cout << el << '\n';
-	}
+	return std::max(std::max(a, b), c);
 }
 
 int main()
 {
-	int n;
-	std::cin >> n;
-	promotie** objs = new promotie * [n];
-	read(n, objs);
-	print_mins(n, objs);
-	/*std::string nume;
-	int durata;
-	int zi, luna, an;
-	int suma;
-	std::getline(std::cin, nume);
-	std::cin >> durata;
-	std::cin >> zi >> luna >> an;
-	std::cin >> suma;
-	promotie obj = promotie(nume, durata, zi, luna, an, suma);
-	std::cout << obj;*/
+	rational a(2, 3), b(1, 2), c(3, 5);
+	std::cout << "max int " << maxim(a, b, c) << std::endl;
 }
 
-std::ostream& operator<<(std::ostream& out, const promotie& obj)
+bool operator<(const rational& self, const rational& other)
 {
-	out << obj.nume << " a inceput in " << obj.zi << '/' <<
-		obj.luna << '/' << obj.an << " si a durat " << obj.durata<<
-		" zile, castig=" << obj.suma;
+	return self.numarator / float(self.numitor) <
+		other.numarator / float(other.numitor);
+}
+
+bool operator==(const rational& self, const rational& other)
+{
+	return self.numarator / float(self.numitor) ==
+		other.numarator / float(other.numitor);
+}
+
+bool operator>(const rational& self, const rational& other)
+{
+	return self.numarator / float(self.numitor) >
+		other.numarator / float(other.numitor);
+}
+
+std::ostream& operator<<(std::ostream& out, const rational& obj)
+{
+	out << obj.numarator << '/' << obj.numitor << '\n';
 	return out;
-}
-
-void change_date(promotie& obj, int zi, int luna, int an)
-{
-	obj.zi = zi;
-	obj.luna = luna;
-	obj.an = an;
 }
